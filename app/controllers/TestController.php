4ca715210b098483916205b7b538727fb72e9620
lamp365 <791845283@qq.com>
2015-06-18 00:23:45 +0800

    laravel4.2

---
+++ app/controllers/TestController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevin
+ * Date: 15-6-12
+ * Time: 上午2:00
+ */
+class TestController extends Controller{
+
+    public function login()
+    {
+        dd('你好');
+    }
+}
---
+++ app/functions.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevin
+ * Date: 15-6-17
+ * Time: 下午10:18
+ */ 
